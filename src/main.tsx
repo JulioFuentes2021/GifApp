87a468ded07f16a058d0cf0c5f18b53c03556ca5
Julio Fuentes <juliodddddd1234@gmail.com>
2021-12-17 12:30:17 -0600

    Building the carousel...

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import GlobalStyles from './components/GlobalStyles'
-import App from './components/Header'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import GlobalStyles from './components/GlobalStyles';
+import App from './components/Carousel';
 import { ApolloClient, HttpLink, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
